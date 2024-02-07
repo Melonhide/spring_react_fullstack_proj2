1c3204fa1dc7a38ae2f8cd18fdee191e87b0e89d
Melonhide <tt2830@columbia.edu>
2024-02-06 22:22:01 -0500

    add student class, and student controller

---
+++ src/main/java/com/tonytao/fullStackProj2/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.tonytao.fullStackProj2.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return List.of(
+                new Student(UUID.randomUUID(),
+                        "James",
+                        "Bond",
+                        "jamesbond@gmail.com",
+                        Student.Gender.MALE
+                ),
+                new Student(UUID.randomUUID(),
+                        "Elisa",
+                        "Tamara",
+                        "elsisatamara@hotmail.com",
+                        Student.Gender.FEMALE
+                )
+        );
+    }
+}
