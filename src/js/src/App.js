164675cd5e47502738e9960214b5cef70c4871f1
Melonhide <tt2830@columbia.edu>
2024-02-10 21:42:17 -0500

    02/10/2024

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import Container from './Container';
 import './App.css';
 import { getAllStudents } from './client'
+import {
+  Table
+} from 'antd';
 
 class App extends Component {
 
@@ -8,16 +12,65 @@ class App extends Component {
     students:[]
   }
 
+  componentDidMount () {
+    this.fetchStudents();
+  }
+
   fetchStudents = ()=>{
     getAllStudents()
     .then(res => res.json()
     .then(students => {
       console.log(students);
+      this.setState({
+        students
+      })
     }));
   }
 
   render(){  
-    return <h1>Hello World Spring Boot & React</h1>
+
+    const{ students } = this.state;
+
+    if (students && students.length){
+      const columns = [
+        {
+          title: 'StudentId',
+          dataIndex: 'studentId',
+          key: 'studentId'
+        },
+        {
+          title: 'First Name',
+          dataIndex: 'firstName',
+          key: 'firstName'
+        },
+        {
+          title: 'Last Name',
+          dataIndex: 'lastName',
+          key: 'lastName'
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email'
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender'
+        }
+      ];
+
+      return (
+      <Container>
+      <Table 
+        dataSource={students} 
+        columns={columns} 
+        rowKey='studentId'/>
+      </Container>
+      );
+    }
+
+    return <h1>No Student Found</h1>
   }
 }
 
