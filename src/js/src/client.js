b845f1a963163a499b0cc74f827bcd87486b969c
Melonhide <tt2830@columbia.edu>
2024-02-12 08:14:50 -0500

    Setup Docker and Create Student Table in Postgre  Database

--- src/js/src/client.js
+++ src/js/src/client.js
@@ -1,4 +1,4 @@
 import fetch from 'unfetch';
 
-export const getAllStudents=()=>fetch('/students');
+export const getAllStudents=()=>fetch('api/students');
 
---
+++ src/main/java/com/tonytao/fullStackProj2/datasource/Datasource.java
@@ -0,0 +1,19 @@
+package com.tonytao.fullStackProj2.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class Datasource {
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
