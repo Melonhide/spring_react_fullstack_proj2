24a1a64c48fa44b5404867170aa68d94ce37b8b9
Tengteng Tao <tengtengtao@Tengtengs-MacBook-Pro.local>
2024-02-15 21:54:33 -0500

    transfrom database info to Java object

--- src/main/java/com/tonytao/fullStackProj2/student/StudentDataAccessService.java
+++ src/main/java/com/tonytao/fullStackProj2/student/StudentDataAccessService.java
@@ -18,9 +18,31 @@ public class StudentDataAccessService {
     }
 
     public List<Student> selectAllStudents(){
-        String sql = "";
+        String sql = "" +
+                "SELECT " +
+                "student_id, " +
+                "first_name, " +
+                "last_name, " +
+                "email, " +
+                "gender " +
+                "FROM student";
         List<Student> students = jdbcTemplate.query(sql,(rs, rowNum) -> {
-            return null;
+            String studentIdStr = resultSet.getString("student_id");
+            UUID studentId = UUID.fromString(studentIdStr);
+
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+            String genderStr = resultSet.getString("gender").toUpperCase();
+
+            Student.Gender gender = Student.Gender.valueOf(genderStr);
+            return new Student(
+                    studentId,
+                    firstName,
+                    lastName,
+                    email,
+                    gender
+            );
         });
         return null;
     }
