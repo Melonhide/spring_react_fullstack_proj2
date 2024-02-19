d84c7d2e416d1091fb304aae67866b5456797090
Melonhide <tt2830@columbia.edu>
2024-02-18 19:06:34 -0500

    add validation rule style; video 037

--- src/js/src/forms/AddStudentForm.js
+++ src/js/src/forms/AddStudentForm.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
-import { Input } from 'antd';
-import { Button } from 'antd';
-
+import { Input, Button, Tag } from 'antd';
 
 const inputBottomMargin = {marginBottom: '10px'};
+const tagStyle = {backgroundColor: '#f50', color: 'white', ...inputBottomMargin };
 
 class AddStudentForm extends Component {
     render() {
@@ -23,7 +22,7 @@ class AddStudentForm extends Component {
                     }                    
 
                     if (!values.email) {
-                        errors.email = 'Required';
+                        errors.email = 'Email Required';
                     } else if (
                         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                     ) {
@@ -64,7 +63,8 @@ class AddStudentForm extends Component {
                             value={values.firstName}
                             placeholder='First name. E.g John'
                         />
-                        {errors.firstName && touched.firstName && errors.firstName}
+                        {errors.firstName && touched.firstName && 
+                            <Tag style={tagStyle}>{errors.firstName}</Tag>}
                         
                         <Input
                             style={inputBottomMargin}
@@ -74,7 +74,8 @@ class AddStudentForm extends Component {
                             value={values.lastName}
                             placeholder='Last name. E.g Wick'
                         />
-                        {errors.lastName && touched.lastName && errors.lastName}
+                        {errors.lastName && touched.lastName && 
+                            <Tag style={tagStyle}>{errors.lastName}</Tag>}
                         
                         <Input
                             style={inputBottomMargin}
@@ -85,7 +86,8 @@ class AddStudentForm extends Component {
                             value={values.email}
                             placeholder='Email. E.g example@gmail.com'
                         />
-                        {errors.email && touched.email && errors.email}
+                        {errors.email && touched.email && 
+                            <Tag style={tagStyle}>{errors.email}</Tag>}
                         
                         <Input
                             style={inputBottomMargin}
@@ -95,7 +97,8 @@ class AddStudentForm extends Component {
                             value={values.gender}
                             placeholder='Gender. E.g Male or Female'
                         />
-                        {errors.gender && touched.gender && errors.gender}
+                        {errors.gender && touched.gender && 
+                            <Tag style={tagStyle}>{errors.gender}</Tag>}
                         
                         <Button type="submit" disabled={isSubmitting}>
                             Submit
