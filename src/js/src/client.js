1eb7d257526bda927ed077eb35b8b70d9b2ada4a
Melonhide <tt2830@columbia.edu>
2024-02-19 22:46:46 -0500

    import add student function in the add student form; video: 041

--- src/js/src/client.js
+++ src/js/src/client.js
@@ -5,7 +5,7 @@ export const getAllStudents=()=>fetch('api/students');
 export const addNewStudent = student => 
     fetch('api/students', {
        headers: {
-            'Content-Type': 'application.json'
+            'Content-Type': 'application/json'
         },
         method: 'POST',
         body: JSON.stringify(student)
