a5ac015738ba260d9d29c41a4a94f11fd76ebf1c
Melonhide <tt2830@columbia.edu>
2024-03-08 22:18:02 -0500

    Add application-test.yml for internal test process during maven build; Successfully build and generate java jar file. This jar file is connected to AWS RDS database, since I did not set up access to database yet, this jar file will raise error for connection failure. Video 085

--- src/test/java/com/tonytao/fullStackProj2/FullStackProj2ApplicationTests.java
+++ src/test/java/com/tonytao/fullStackProj2/FullStackProj2ApplicationTests.java
@@ -2,8 +2,9 @@ package com.tonytao.fullStackProj2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+@ActiveProfiles("test")
 class FullStackProj2ApplicationTests {
 
 	@Test
